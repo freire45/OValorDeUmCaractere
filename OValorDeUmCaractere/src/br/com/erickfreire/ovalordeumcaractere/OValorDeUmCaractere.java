22a1be3ff2a7b4a585759c1ec8f931cba4922d7a
Erick Freire <erilefr@gmail.com>
2021-03-27 23:15:58 -0300

    OValorDeUmCaractere
    OValorDeUmCaractere

---
+++ OValorDeUmCaractere/src/br/com/erickfreire/ovalordeumcaractere/OValorDeUmCaractere.java
@@ -0,0 +1,27 @@
+package br.com.erickfreire.ovalordeumcaractere;
+
+
+/**
+ * Crie um programa em Java que recebe um caractere e mostra o valor dele na tabela ASCII
+ * @author Erick Freire
+ * @version 1 - Criado em 27-03-2021 as 23:11
+ */
+
+public class OValorDeUmCaractere {
+	public static void main(String[] args) {
+		System.out.printf("o Caractere %c tem o valor %d%n", 'A', ((int)'A'));
+		System.out.printf("o Caractere %c tem o valor %d%n", 'B', ((int)'B'));
+		System.out.printf("o Caractere %c tem o valor %d%n", 'C', ((int)'C'));
+		System.out.printf("o Caractere %c tem o valor %d%n", 'a', ((int)'a'));
+		System.out.printf("o Caractere %c tem o valor %d%n", 'b', ((int)'b'));
+		System.out.printf("o Caractere %c tem o valor %d%n", 'c', ((int)'c'));
+		System.out.printf("o Caractere %c tem o valor %d%n", '0', ((int)'0'));
+		System.out.printf("o Caractere %c tem o valor %d%n", '1', ((int)'1'));
+		System.out.printf("o Caractere %c tem o valor %d%n", '2', ((int)'2'));
+		System.out.printf("o Caractere %c tem o valor %d%n", '*', ((int)'*'));
+		System.out.printf("o Caractere %c tem o valor %d%n", '+', ((int)'+'));
+		System.out.printf("o Caractere %c tem o valor %d%n", '/', ((int)'/'));
+		System.out.printf("o Caractere %c tem o valor %d%n", ' ', ((int)' '));
+	}
+
+}
